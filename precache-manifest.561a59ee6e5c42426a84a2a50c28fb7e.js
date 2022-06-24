0b3b07757a7650302980b79fd77c50d3d6333da5
walfindobayusetya <walfindobayusetya23.wbs@gmail.com>
2022-06-24 12:08:38 +0700

    Updates

---
+++ precache-manifest.561a59ee6e5c42426a84a2a50c28fb7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af767a05e9af6d2d09d57d76fce4c002",
+    "url": "/reactjs-todolist/index.html"
+  },
+  {
+    "revision": "4d0d51165ed751051c2f",
+    "url": "/reactjs-todolist/static/css/main.61b4c374.chunk.css"
+  },
+  {
+    "revision": "aac8a1efb0aacd2e3829",
+    "url": "/reactjs-todolist/static/js/2.349ce499.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-todolist/static/js/2.349ce499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d0d51165ed751051c2f",
+    "url": "/reactjs-todolist/static/js/main.7987b72b.chunk.js"
+  },
+  {
+    "revision": "0fa1a54836b3ed49d4e8",
+    "url": "/reactjs-todolist/static/js/runtime-main.76b17496.js"
+  }
+]);
